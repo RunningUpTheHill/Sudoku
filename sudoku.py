d00ebc88fa179a7c7c1ad03d1473e0b89942f366
Edward Kempa <53019149+Eddi005@users.noreply.github.com>
2022-12-04 11:39:27 -0500

    #Fixed bug where sketch could draw into any cell if mouse was moved after selecting

--- sudoku.py
+++ sudoku.py
@@ -167,11 +167,11 @@ class Board:
         cur_board.board[y][x] = 0
         self.cell = [[Cell(self.board[i][j], i, j, cell_size, cell_size) for j in range(cols)] for i in range(rows)]
 
-    def sketch(self):  # sketches the user's input but draws it in maroon color to distinguish
-        pos = pygame.mouse.get_pos()
+    def sketch(self, pos):  # sketches the user's input but draws it in maroon color to distinguish
         cor_x, cor_y = pos[1] // cell_size, pos[0] // cell_size
         sketch_value = read_number()
-        self.cell[cor_x][cor_y] = Cell(self.board[cor_x][cor_y], cor_x, cor_y, cell_size, cell_size, sketch_value)
+        if sketch_value != 0:
+            self.cell[cor_x][cor_y] = Cell(self.board[cor_x][cor_y], cor_x, cor_y, cell_size, cell_size, sketch_value)
 
     def place_number(self):  # user establishes their choice, this function is called when user hits enter
         choice = read_number()
@@ -362,7 +362,10 @@ if __name__ == "__main__":
 
                         elif event.key != pygame.K_s:
                             if mode_sketch:
-                                cur_board.sketch()
+                                try:
+                                    cur_board.sketch(pos)
+                                except:
+                                    pass
                             else:
                                 cur_board.place_number()
 
@@ -372,6 +375,7 @@ if __name__ == "__main__":
                 try:
                     if event.type == pygame.MOUSEBUTTONDOWN:
                         if game_on:
+                            pos = pygame.mouse.get_pos()
                             y, x = cur_board.click()
                             selected = False
                             if (y <= 8) and cur_board.tupl_board[0][y][x] == 0:
